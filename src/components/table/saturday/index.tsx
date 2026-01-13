cb7da54cb0bc7b93c002cb845aa21c1b7ef7c86c
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-01-13 07:17:13 -0300

    finish saturday

--- src/components/table/saturday/index.tsx
+++ src/components/table/saturday/index.tsx
@@ -1,11 +1,19 @@
-import type { SaturdayRow } from "@/constants"
+import type { Bulletin, KeysOfUnion, SaturdayRow } from "@/constants"
 import { formatDay } from "@/utils/format-day";
+import { useContext } from "react";
+import { TableContext } from "../provider";
 
 type Props = {
   rows: SaturdayRow[];
 }
 
 export function SaturdayTable({ rows }: Props) {
+  const { changeCategoryField } = useContext(TableContext);
+
+  function changeField(day: number, field: KeysOfUnion<Bulletin['days']['saturday'][0]>) {
+    return (e: React.ChangeEvent<HTMLInputElement>) => changeCategoryField('saturday', field, day, e.target.value);
+  }
+
   return (
     <table>
       <thead>
@@ -23,16 +31,31 @@ export function SaturdayTable({ rows }: Props) {
       </thead>
       <tbody>
         {rows.map((row) => (
-          <tr key={row.day}>
+          <tr key={row.day} className="relative">
             <td data-align="center">{formatDay(row.day)}</td>
             <td>
-              <input placeholder="digite aqui..." className="w-full h-full min-w-[0px]" />
+              <input
+                placeholder="digite aqui..."
+                className="w-full h-full min-w-[0px]"
+                defaultValue={row.facilitator}
+                onChange={changeField(row.day, "facilitator")}
+              />
             </td>
             <td>
-              <input placeholder="digite aqui..." className="w-full h-full min-w-[0px]" />
+              <input
+                placeholder="digite aqui..."
+                className="w-full h-full min-w-[0px]"
+                defaultValue={row.theme}
+                onChange={changeField(row.day, "theme")}
+              />
             </td>
             <td>
-              <input placeholder="digite aqui..." className="w-full h-full min-w-[0px]" />
+              <input
+                placeholder="digite aqui..."
+                className="w-full h-full min-w-[0px]"
+                defaultValue={row.divineCult}
+                onChange={changeField(row.day, "divineCult")}
+              />
             </td>
           </tr>
         ))}
